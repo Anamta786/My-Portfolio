4bb1ce14a5fb7dd4c6d1d57c3b119b0458794790
Anamta Khan <khananamta22@gmail.com>
2020-06-05 15:49:57 +0530

    Build removed from gitignore

---
+++ build/precache-manifest.97c0181588d623ad924ae5b1755baaf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf368f309a4653aacc71b1e0debbf9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16424a0b7de07c968756",
+    "url": "/static/css/main.c7999b87.chunk.css"
+  },
+  {
+    "revision": "8f4367272d7796ffb9aa",
+    "url": "/static/js/2.fe12cbe6.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.fe12cbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16424a0b7de07c968756",
+    "url": "/static/js/main.80d180a0.chunk.js"
+  },
+  {
+    "revision": "d12dd9bf05d36c957749",
+    "url": "/static/js/runtime-main.5445ad03.js"
+  }
+]);
