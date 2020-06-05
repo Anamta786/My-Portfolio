2a42c3cde76fbbf7d8be7b22f9017d9956fe692e
Anamta Khan <khananamta22@gmail.com>
2020-06-05 16:14:17 +0530

    Changes #2

---
+++ build/precache-manifest.06f4592e8f010002fbf6ca7db16ffa64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb4f7fdc2f8ed9f9133301bc23abc01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b208cb49912eb171755e",
+    "url": "/static/css/main.c7999b87.chunk.css"
+  },
+  {
+    "revision": "8f4367272d7796ffb9aa",
+    "url": "/static/js/2.fe12cbe6.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.fe12cbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b208cb49912eb171755e",
+    "url": "/static/js/main.c5033579.chunk.js"
+  },
+  {
+    "revision": "d12dd9bf05d36c957749",
+    "url": "/static/js/runtime-main.5445ad03.js"
+  }
+]);
